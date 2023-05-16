479edf8ea44568936825f7140920c9d4a3c0e1f4
Prince <princemehta516@gmail.com>
2023-05-16 21:41:18 +0530

    add Newsletter component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Analytics from "./components/Analytics";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import NewsLetter from "./components/NewsLetter";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Navbar />
       <Hero />
       <Analytics />
+      <NewsLetter />
     </div>
   );
 }
