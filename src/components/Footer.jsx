9de6f584773571b8f9f2a5a4a1bc6b1f76e1eacf
Prince <princemehta516@gmail.com>
2023-05-18 21:32:32 +0530

    add card components

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { FaDribbleSquare,
+         FaFacebookSquare,
+         FaGithubSquare,
+         FaInstagramSquare,
+         FaTwitterSquare
+} from 'react-icons/fa';
+
+const Footer = () => {
+  return (
+    <div>
+        Footer
+    </div>
+  )
+}
+
+export default Footer
