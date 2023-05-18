d26b459e4a0108404576018544d85f97ac04ecdc
Prince <princemehta516@gmail.com>
2023-05-18 21:27:20 +0530

    Add Cards component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import Analytics from "./components/Analytics";
+import Banner from "./components/Banner";
+import Cards from "./components/Cards";
+import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import NewsLetter from "./components/NewsLetter";
@@ -10,8 +13,12 @@ function App() {
       <Hero />
       <Analytics />
       <NewsLetter />
+      <Cards />
+      {/* <Banner /> */}
+      <Footer />
     </div>
   );
 }
 
 export default App;
+ 
